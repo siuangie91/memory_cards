6949e77c46645a939e7e8f6f7597452b019297f4
siuangie91 <siuangie91@gmail.com>
2018-06-19 18:18:19 -0400

    refactor Choices as a functional component

--- src/components/Choices/Choices.js
+++ src/components/Choices/Choices.js
@@ -2,19 +2,17 @@ import React from 'react';
 
 import styles from './Choices.scss';
 
-class Choices extends React.Component {
-	render() {
-		const btn1Classes = (this.props.hasOwnProperty('btn1Classes')) ? this.props.btn1Classes : "";
-		const btn2Classes = (this.props.hasOwnProperty('btn2Classes')) ? this.props.btn2Classes : "";
+const Choices = (props) => {
+	const btn1Classes = (props.hasOwnProperty('btn1Classes')) ? props.btn1Classes : "";
+	const btn2Classes = (props.hasOwnProperty('btn2Classes')) ? props.btn2Classes : "";
 
-		return (
-			<div className={styles.choices}>
-		    <p>{this.props.text}</p>	
-		    <button className={`btn ${btn1Classes}`} onClick={() => this.props.clickHandler(this.props.btn1Text)}>{this.props.btn1Text}</button>
-		    <button className={`btn ${btn2Classes}`} onClick={() => this.props.clickHandler(this.props.btn2Text)}>{this.props.btn2Text}</button>
-			</div>
-		);
-	}
-}
+	return (
+		<div className={styles.choices}>
+	    <p>{props.text}</p>	
+	    <button className={`btn ${btn1Classes}`} onClick={() => props.clickHandler(props.btn1Text)}>{props.btn1Text}</button>
+	    <button className={`btn ${btn2Classes}`} onClick={() => props.clickHandler(props.btn2Text)}>{props.btn2Text}</button>
+		</div>
+	);
+};
 
 export default Choices;
